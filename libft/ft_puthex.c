38177a038db648b078c446638969ef24b9347889
Beatriz-Chagas <bchagas440@gmail.com>
2025-10-28 23:46:00 -0300

    Atualizando a libft: printf e puthex

--- libft/ft_puthex.c
+++ libft/ft_puthex.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_libft.h"
 
 int	ft_puthex(unsigned long n, char upper)
 {
